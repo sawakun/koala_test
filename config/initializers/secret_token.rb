9d8936d5c34f17ff9d11a98750af2528aecd6cd3
Yohei Sawayama <yohei.sawayama@gmail.com>
2015-05-06 17:27:40 +0900

    add koala

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+KoalaTest::Application.config.secret_key_base = '20f72a50e2af417c932c1d46c8fcaa7b5915d966cd1b33f862700795fbe87b018c859ef6ea3b4db5fb94571673b3491f1f77e912f9cc1b2e7f6d655f708da2e2'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+KoalaTest::Application.config.session_store :cookie_store, key: '_koala_test_session'
